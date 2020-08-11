bf62d8da2aced8d2e3e56d48e55a64fe9d5ef511
odelucca <delucca@pm.me>
2020-08-11 12:44:16 -0300

    :bug: Fixing

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PublicRoute from 'components/Routes/Public';
 
-import Login from './Login';
+import Login from './login';
 
 const Index = () => (
   <PublicRoute>
